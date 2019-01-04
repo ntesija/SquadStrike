12d74ed51265e4ae06725c1f797ecb6e60ca1e8c
Nik Tesija <ntesija@umich.edu>
2019-01-03 20:09:16 -0500

    Updates

---
+++ precache-manifest.2c313289121accb78d39dbbcae2e94c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b5b36824cd40c38cd27",
+    "url": "/SquadStrike/static/js/runtime~main.3b5b3682.js"
+  },
+  {
+    "revision": "9c13648a5b48e253deca",
+    "url": "/SquadStrike/static/js/main.9c13648a.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/SquadStrike/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "9c13648a5b48e253deca",
+    "url": "/SquadStrike/static/css/main.0616cb42.chunk.css"
+  },
+  {
+    "revision": "7ad7983e253a33a26273b237a2f1a344",
+    "url": "/SquadStrike/index.html"
+  }
+];
